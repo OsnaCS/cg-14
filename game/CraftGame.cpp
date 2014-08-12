1273bdbdc613c0887a8e0555e0951598a68c4dab
Alexander Ditt <aditt@uos.de>
2014-08-12 16:04:18 +0200

    Fehlerbehebung und Move in View Direction

--- game/CraftGame.cpp
+++ game/CraftGame.cpp
@@ -30,7 +30,7 @@ void CraftGame::init() {
   m_window.addEventCallback([&](InputEvent e) {
     // if the inputType was a KeyInput and the key was just pressed and the
     // key was Escape -> set m_running to false to stop program
-
+  return m_camera.processEvent(e, m_window);
     if (e.type == InputType::KeyPressed || e.type == InputType::KeyHold) {
 
       auto pos = m_camera.getPosition();
@@ -72,7 +72,7 @@ void CraftGame::init() {
           }
           break;
         default:
-          return m_camera.processEvent(e);
+          
           break;
       }
     }
