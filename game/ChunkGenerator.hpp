e64210a4d4fd72befbbe0e5a8da815c31a2515d1
Stephanie Heyderich <sheyderich@uos.de>
2014-08-12 16:11:34 +0200

    formatted

--- game/ChunkGenerator.hpp
+++ game/ChunkGenerator.hpp
@@ -6,26 +6,25 @@
 #include "Map.hpp"
 
 
-using namespace std; 
+using namespace std;
 
-class ChunkGenerator{
+class ChunkGenerator {
 public:
+  ChunkGenerator();
 
-	ChunkGenerator();
+  // generiert den Chunk auf dem der Player startet
+  void chunkGenerationPrime(Map m);
 
-	//generiert den Chunk auf dem der Player startet
-	void chunkGenerationPrime(Map m);
+  // generiert alle weiteren Chunks abhängig davon wo der Player hinläuft
+  void chunkGeneration(Map m, Vec3i player_pos);
 
-	//generiert alle weiteren Chunks abhängig davon wo der Player hinläuft
-	void chunkGeneration(Map m, Vec3i player_pos);
+  // berechnet den Perlin Noise Wert (eine randomisierte Höhe)
+  int perlinNoise(int x, int z);
 
-	//berechnet den Perlin Noise Wert (eine randomisierte Höhe)
-	int perlinNoise(int x, int z);
-
-	float randomPos(Vec2i vector);
+  float randomPos(Vec2i vector);
 
 private:
-	long m_seed;
-	int m_octave;
-	//random
+  long m_seed;
+  int m_octave;
+  // random
 };
