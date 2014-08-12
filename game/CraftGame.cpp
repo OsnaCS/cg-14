2e1f5ca10b9d3a4b4a1e4525402697ed02a0fbb2
Garret Hohmann <ghohmann@cip48.informatik.uni-osnabrueck.de>
2014-08-12 17:12:37 +0200

    Camera update, Controll update, Shader update

--- game/CraftGame.cpp
+++ game/CraftGame.cpp
@@ -9,13 +9,13 @@ using namespace std;
 
 CraftGame::CraftGame() {
   m_running = true;
-  m_map.addChunk(Vec2i(0,0));
-  
+  m_map.addChunk(Vec2i(0, 0));
+
   for(int i = 0; i < 16; i++) {
     m_map.setBlockType(Vec3i(i, 0, i), BlockType::Dirt);
-    m_map.setBlockType(Vec3i(15-i, 0, i), BlockType::Dirt);
-  } 
-} 
+    m_map.setBlockType(Vec3i(15 - i, 0, i), BlockType::Dirt);
+  }
+}
 
 
 void CraftGame::init() {
@@ -24,47 +24,15 @@ void CraftGame::init() {
   m_window.setVersionHint(3, 3);
 
   // add event callback (capture by reference
+  m_window.addEventCallback(
+    [&](InputEvent e) { return m_camera.processEvent(e, m_window); });
+
   m_window.addEventCallback([&](InputEvent e) {
     // if the inputType was a KeyInput and the key was just pressed and the
     // key was Escape -> set m_running to false to stop program
-  return m_camera.processEvent(e, m_window);
-    if (e.type == InputType::KeyPressed || e.type == InputType::KeyHold) {
-
-      auto pos = m_camera.getPosition();
-
-      switch (e.keyInput.key) {
-        case KeyCode::Escape:
-          m_running = false;
-          return EventResult::Processed;
-          break;
-        case KeyCode::Up:
-          pos.y += 0.5f;
-          m_camera.setPosition(pos);
-          break;
-        case KeyCode::Down:
-          pos.y -= 0.5f;
-          m_camera.setPosition(pos);
-          break;
-        case KeyCode::Left:
-          pos.x -= 0.5f;
-          m_camera.setPosition(pos);
-          break;
-        case KeyCode::Right:
-          pos.x += 0.5f;
-          m_camera.setPosition(pos);
-          break;
-        case KeyCode::W:
-          pos.z += 0.5f;
-          m_camera.setPosition(pos);
-          break;
-        case KeyCode::S:
-          pos.z -= 0.5f;
-          m_camera.setPosition(pos);
-          break;
-        default:
-          
-          break;
-      }
+    if(e.type == InputType::KeyPressed && e.keyInput.key == KeyCode::Escape) {
+      m_running = false;
+      return EventResult::Processed;
     }
     return EventResult::Skipped;
   });
@@ -79,9 +47,8 @@ void CraftGame::start() {
   auto& renderContext = m_window.getRenderContext();
   renderContext.create();
   // we just need one context, so we can prime it here just once
-  renderContext.prime([this](HotRenderContext& hotContext) {
-    this->run(hotContext);
-  });
+  renderContext.prime(
+    [this](HotRenderContext& hotContext) { this->run(hotContext); });
 }
 
 void CraftGame::run(lumina::HotRenderContext& hotContext) {
@@ -102,16 +69,23 @@ void CraftGame::run(lumina::HotRenderContext& hotContext) {
 
   auto now = chrono::system_clock::now();
 
+  uint counter = 0;
+
   // run as long as the window is valid and the user hasn't pessed ESC
   while(m_running && m_window.isValid()) {
     auto diff = chrono::system_clock::now() - now;
-    float s = chrono::duration_cast<chrono::milliseconds>(diff).count() / 1000.f;
-    slog(s, ", FPS:", 1/s);
+    float s = chrono::duration_cast<chrono::milliseconds>(diff).count()
+              / 1000.f;
+
+    if(counter % 100 == 0) {
+      slog("FPS:", 1 / s);
+    }
+
     now = chrono::system_clock::now();
 
     // poll events
     m_window.update();
-    
+
     // we need the default FrameBuffer
     hotContext.getDefaultFrameBuffer().prime([&](HotFrameBuffer& hotFB) {
       // clear the background color of the screen
@@ -124,14 +98,17 @@ void CraftGame::run(lumina::HotRenderContext& hotContext) {
         hot.uniform["u_view"] = this->m_camera.get_matrix();
         hot.uniform["u_projection"] = this->m_camera.get_ProjectionMatrix();
 
-        Chunk& currentChunk = m_map.getChunk(Vec2i(0,0));
-        ChunkView cV(currentChunk,Vec2i(0,0));
+        Chunk& currentChunk = m_map.getChunk(Vec2i(0, 0));
+        ChunkView cV(currentChunk, Vec2i(0, 0));
 
         cV.draw(hot);
 
       });
     });
+    
     // swap buffer
     hotContext.swapBuffer();
+
+    counter++;
   }
 }
