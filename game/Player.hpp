018a463bede2ca28541f142ecca2e3a1a1612454
Stephanie Heyderich <sheyderich@uos.de>
2014-08-12 10:32:21 +0200

    Added first version of player

---
+++ game/Player.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "lumina/lumina.hpp"
+using namespace std;
+
+
+class Player {
+public:
+
+	//Konstruktor
+	Player();
+
+	//Setter-Methoden
+	setPos(int x, int y, int z){
+		m_pos = Vec3i(x,y,z);
+	}
+
+	//getter-Methoden
+  Vec3i getPos(){
+		return m_pos;
+	}
+
+	int getHealth(){
+		return m_health;
+	}
+
+	int m_hunger(){
+		return m_hunger;
+	}
+
+
+
+private:
+	Vec3i m_pos;
+	int m_health;
+	int m_hunger;
+	Inventar m_inventar;
+}
