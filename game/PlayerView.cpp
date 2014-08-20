4523d6ab3e2d74b8f5735908b3fb3cd79b307f62
Alexander Ditt <aditt@uos.de>
2014-08-20 15:07:19 +0200

    Implemented the update possibility of hearts

--- game/PlayerView.cpp
+++ game/PlayerView.cpp
@@ -14,11 +14,11 @@ m_player(player)
  
 void PlayerView::draw()
 {
-
+  updateHearts();
 	m_program.prime([&](HotProgram& hotprog)
 	{
  		m_colorTexture.prime(0, [&](HotTex2D& hotTex) {
-			hotprog.draw(hotTex, m_heartPanel, PrimitiveType::TriangleStrip);
+			hotprog.draw(hotTex, updateHearts(), PrimitiveType::TriangleStrip);
 		});
 	});
 }
@@ -26,6 +26,7 @@ void PlayerView::draw()
 
 void PlayerView::init()
 {
+
 	//Initialize of the texture
 	ImageBox image_box = loadJPEGImage("gfx/hearts.jpeg");
   m_colorTexture.create(Vec2i(256,128), TexFormat::RGB8, image_box.data());
@@ -36,38 +37,41 @@ void PlayerView::init()
 	vs.compile(loadShaderFromFile("shader/HeartPanel.vsh"));
 	FShader fs;
 	fs.compile(loadShaderFromFile("shader/HeartPanel.fsh"));
+  // create program and link the two shaders
+	m_program.create(vs, fs);
+		m_program.perFragProc.blendFuncRGB = BlendFunction::Add;
+	m_program.perFragProc.srcRGBParam = BlendParam::SrcAlpha;
+	m_program.perFragProc.dstRGBParam = BlendParam::OneMinusSrcAlpha;
+	
+}
 
+VertexSeq PlayerView::updateHearts(){
+  
   int activeHearts = m_player.getHearts();
   int maxHearts = m_player.getMaxHearts();
   float nextHeart = -(maxHearts / 2.0f) * HEART_SIZE;
-
-  // create program and link the two shaders
-	m_program.create(vs, fs);
-	m_program.perFragProc.blendFuncRGB = BlendFunction::Add;
-	m_program.perFragProc.srcRGBParam = BlendParam::SrcAlpha;
-	m_program.perFragProc.dstRGBParam = BlendParam::OneMinusSrcAlpha;
-
-  m_heartPanel.create(7, 4 * maxHearts, 5 * maxHearts);
+ 	VertexSeq m_heartPanel;
+	m_heartPanel.create(7, 4 * maxHearts, 5 * maxHearts);
   m_heartPanel.prime<Vec2f, Vec3f, Vec2f>([&](HotVertexSeq<Vec2f, Vec3f, Vec2f>& hot)
   {
-	  	for(int i = 0; i < maxHearts; i++){
-	  		if(activeHearts > i){
-			  	hot.vertex[0+i*4].set(Vec2f(nextHeart +	HEART_SIZE * i, HEART_POSY), Vec3f(0,1,1), Vec2f(0.5,0));
-			  	hot.vertex[1+i*4].set(Vec2f(nextHeart +	HEART_SIZE * i, HEART_POSY - HEART_SIZE), Vec3f(1,1,1),Vec2f(0.5,1));
-			  	hot.vertex[2+i*4].set(Vec2f(nextHeart + HEART_SIZE * (i + 1), HEART_POSY), Vec3f(1,1,0),Vec2f(1,0));
-			  	hot.vertex[3+i*4].set(Vec2f(nextHeart + HEART_SIZE * (i + 1), HEART_POSY - HEART_SIZE), Vec3f(1,0,1),Vec2f(1,1));
-		  	}else{
-			  	hot.vertex[0+i*4].set(Vec2f(nextHeart +	HEART_SIZE * i, HEART_POSY), Vec3f(0,1,1), Vec2f(0.0,0));
-			  	hot.vertex[1+i*4].set(Vec2f(nextHeart +	HEART_SIZE * i, HEART_POSY - HEART_SIZE), Vec3f(1,1,1),Vec2f(0.0,1));
-			  	hot.vertex[2+i*4].set(Vec2f(nextHeart + HEART_SIZE * (i + 1), HEART_POSY), Vec3f(1,1,0),Vec2f(0.5,0));
-			  	hot.vertex[3+i*4].set(Vec2f(nextHeart + HEART_SIZE * (i + 1), HEART_POSY - HEART_SIZE), Vec3f(1,0,1),Vec2f(0.5,1));
-		  	}
-	  		hot.index[0+ i * 5] = 0 + i * 4;
-	  		hot.index[1+ i * 5] = 1 + i * 4;
-	  		hot.index[2+ i * 5] = 2 + i * 4;
-	  		hot.index[3+ i * 5] = 3 + i * 4;
-	  		hot.index[4+ i * 5] = GLIndex::PrimitiveRestart;
-	  	}
-
-		});
+ 	for(int i = 0; i < maxHearts; i++){
+	  if(activeHearts > i){
+		 	hot.vertex[0+i*4].set(Vec2f(nextHeart +	HEART_SIZE * i, HEART_POSY), Vec3f(0,1,1), Vec2f(0.5,0));
+	  	hot.vertex[1+i*4].set(Vec2f(nextHeart +	HEART_SIZE * i, HEART_POSY - HEART_SIZE), Vec3f(1,1,1),Vec2f(0.5,1));
+	  	hot.vertex[2+i*4].set(Vec2f(nextHeart + HEART_SIZE * (i + 1), HEART_POSY), Vec3f(1,1,0),Vec2f(1,0));
+	  	hot.vertex[3+i*4].set(Vec2f(nextHeart + HEART_SIZE * (i + 1), HEART_POSY - HEART_SIZE), Vec3f(1,0,1),Vec2f(1,1));
+	 	}else{
+		 	hot.vertex[0+i*4].set(Vec2f(nextHeart +	HEART_SIZE * i, HEART_POSY), Vec3f(0,1,1), Vec2f(0.0,0));
+	  	hot.vertex[1+i*4].set(Vec2f(nextHeart +	HEART_SIZE * i, HEART_POSY - HEART_SIZE), Vec3f(1,1,1),Vec2f(0.0,1));
+		 	hot.vertex[2+i*4].set(Vec2f(nextHeart + HEART_SIZE * (i + 1), HEART_POSY), Vec3f(1,1,0),Vec2f(0.5,0));
+		 	hot.vertex[3+i*4].set(Vec2f(nextHeart + HEART_SIZE * (i + 1), HEART_POSY - HEART_SIZE), Vec3f(1,0,1),Vec2f(0.5,1));
+		}
+	  hot.index[0+ i * 5] = 0 + i * 4;
+	  hot.index[1+ i * 5] = 1 + i * 4;
+	 	hot.index[2+ i * 5] = 2 + i * 4;
+		hot.index[3+ i * 5] = 3 + i * 4;
+		hot.index[4+ i * 5] = GLIndex::PrimitiveRestart;
+	}
+	});
+	return m_heartPanel;
 }
