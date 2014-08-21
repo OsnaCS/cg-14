45980c6e5ce75858d9e5d4809c837c64fe1dfe00
Johan von Behren <johan@vonbehren.eu>
2014-08-21 11:44:43 +0200

    Buildskript angepasst

--- build.sh
+++ build.sh
@@ -5,11 +5,8 @@ TARGET="$1"
 clear && clear
 if [ "$TARGET" == "d" ]; then
 	echo "==== building CraftGame in debug mode ===="
-	make ASAN=1 -j4
+	make ASAN=1 -j4 && echo "==== running CraftGame ====" && ./app
 else
 	echo "==== building CraftGame in release mode ===="
-	make RELEASE=1 -j4
+	make RELEASE=1 -j4 && echo "==== running CraftGame ====" && ./app
 fi
-
-echo "==== running CraftGame ===="
-./app
